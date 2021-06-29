bda80a55516957d2b55d7fb2ba5512391bdd114d
cjohnson714 <cjohnson714@gmail.com>
2021-06-29 23:17:47 +0000

    Hacky tray stuff for multi-monitor and module fixes for Polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,7 +2,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 #polybar top &
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload top &
+    MONITOR=$m
+    if [ "$m" == 'DP-0' ]; then
+        MONITOR=$m TRAY_POS=right polybar --reload top &
+    else
+        MONITOR=$m TRAY_POS=none polybar --reload top &
+    fi
 done
 
 
