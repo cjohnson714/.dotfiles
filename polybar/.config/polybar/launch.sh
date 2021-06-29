0629e4fb30d57ceda6669312f5cdf23c0f33d980
cjohnson714 <cjohnson714@gmail.com>
2021-06-29 06:56:16 +0000

    Multi-monitor support

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,5 +1,9 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar top &
+#polybar top &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload top &
+done
+
 
 echo "Bars launched..."
